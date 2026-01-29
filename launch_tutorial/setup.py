e58c6a0d6467d427f11ad11ba8aac6f68d3d9d09
Zhicun Tan <zhicun.tan@kiongroup.com>
2026-01-29 19:45:14 +0800

    update on launch_tutorial

--- launch_tutorial/setup.py
+++ launch_tutorial/setup.py
@@ -1,14 +1,29 @@
-import os
-from glob import glob
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'launch_tutorial'
 
 setup(
-    # Other parameters ...
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
     data_files=[
-        # ... Other data files
-        # Include all launch files.
-        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
-    ]
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='zhicun',
+    maintainer_email='zhicun.tan@kiongroup.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
 )
