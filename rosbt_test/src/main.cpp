dce6180b4da8d0feccd6d1a60072d96826d55bcd
Zhicun Tan <tanzc9866@126.com>
2026-08-24 13:21:37 +0800

    restructure

--- rosbt_test/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <rclcpp/rclcpp.hpp>
-#include <example_interfaces/srv/add_two_ints.hpp>
-
-using AddTwoInts = example_interfaces::srv::AddTwoInts;
-
-int main(int argc, char** argv)
-{
-    rclcpp::init(argc, argv);
-}
